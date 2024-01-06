d3b3773546803a5b1d61d58ac4f84ddfa94f9645
ARUNKUMARMARU <130632965+ARUNKUMARMARU@users.noreply.github.com>
2024-01-06 17:21:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Home from '../component/Home'
-import Signup from '../component/signup'
+import Signup from '../component/SignUp'
 import Reset from '../component/Reset'
 
 
