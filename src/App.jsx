48d9885bdda03ae26374f8981533d79b1ece17fb
ARUNKUMARMARU <arunsmart1020@gmail.com>
2024-01-08 17:00:32 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Home from './component/Home'
-// import SignUp from './component/SignUp'
 import Sign from './component/Sign'
 import Reset from './component/Reset'
 
